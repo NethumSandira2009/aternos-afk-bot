376cd0d3db1fc52791a7f94715014eb20110c1d6
NethumSandira2009 <pdomadhavee@gmail.com>
2025-11-29 13:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const mineflayer = require("mineflayer");
 
 function startBot() {
   const bot = mineflayer.createBot({
-    host: process.env.HOST,  // Railway will set this
+    host: process.env.HOST,      // Railway will insert your Aternos IP
     username: "AFK_Bot",
-    version: false
+    version: "1.16.5"            // <-- IMPORTANT: set MC version here
   });
 
   bot.on("spawn", () => {
-    console.log("Bot connected!");
+    console.log("Bot connected to 1.16.5 server!");
     bot.setControlState("sneak", true);
   });
 
